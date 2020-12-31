dcf03c2b59e376f5177b0c6d7e83b7c377f485e2
ssbmars <46971717+ssbmars@users.noreply.github.com>
2020-12-30 18:18:52 -0800

    Update bbn3_netplay.lua

--- BizHawk-2.5.2/bbn3_netplay.lua
+++ BizHawk-2.5.2/bbn3_netplay.lua
@@ -1,9 +1,30 @@
+rollbackmode = 1	--toggle this between 1 and nil
+saferollback = 6
+resimulating = nil
+HideResim = 1
+savcount = 30	--amount of savestate frames to keep
+client.displaymessages(false)
+
 InputData = 0x0203B400
 InputBufferLocal = InputData
 InputBufferRemote = InputData + 0x1
-InputStackSize = InputData + 0x3
+InputStackSize = InputData + 0x2
 InputStackLocal = InputData + 0x10
 InputStackRemote = InputData + 0x18
+SceneIndicator = 0x020097F8
+PlayerHPLocal = 0x02037294
+PlayerHPRemote = 0x02037368
+
+FullInputStack = {}  --input stack for rollback frames
+sav = {}  --savestate ID table
+
+--define variables for gui.draw
+	--things get drawn at the base GBA resolution and then scaled up, so calculations are based on 1x GBA res
+x_center = 120 
+y_center = 80
+
+
+
 
 PLAYERNUM = 0
 HOST_IP = "127.0.0.1"
@@ -13,7 +34,7 @@ socket = require("socket.core")
 
 tcp = nil
 connected = nil
-client = nil
+connectedclient = nil
 t = {}
 c = {}
 ctrl = {}
@@ -30,10 +51,23 @@ opponent = nil
 acked = false
 timedout = 0
 
+
 menu = nil
+local delaymenu = 20
+
+while delaymenu > 0 do
+	delaymenu = delaymenu - 1
+	emu.frameadvance()
+end
+
+local windowsize = client.getwindowsize()
+local form_xpos = (client.xpos() + 120*windowsize - 142)
+local form_ypos = (client.ypos() + 80*windowsize + 10)
+
 menu = forms.newform(300,140,"BBN3 Netplay",function()
 	return nil
 end)
+forms.setlocation(menu, form_xpos , form_ypos)
 label_ip = forms.label(menu,"IP:",8,0,32,24)
 port_ip = forms.label(menu,"Port:",8,30,32,24)
 textbox_ip = forms.textbox(menu,"127.0.0.1",240,24,nil,40,0)
@@ -67,10 +101,32 @@ local function custsynchro()
 			end
 		end
 		
+		-- Sync Player HP and Input Buffer
+		if type(l) == "table" and #l > 0 then
+			memory.write_u8(InputBufferRemote, l[3])
+			if PLAYERNUM == 1 and #l >= 6 then
+				memory.write_u16_le(PlayerHPRemote, l[6])
+			elseif PLAYERNUM == 2 and #l >= 6 then
+				memory.write_u16_le(PlayerHPLocal, l[6])
+			--	memory.write_u32_le(0x02009730, l[7])
+			--	memory.write_u32_le(0x02009800, l[8])
+			end
+		end
+		
+		-- Rewrite Client's Timestamp
+		if PLAYERNUM == 2 then
+			if type(c) == "table" and #c > 0 then
+				if type(c[1]) == "table" and #c[1] > 0 then
+					if c[1][3] == 0x4 or memory.read_u8(SceneIndicator) == 0x4 then
+						memory.write_u16_le(0x0203b380, l[9])
+					end
+				end
+			end
+		end
+		
 		if reg3 == 0x2 then
 			waitingforround = 1
 			if type(l) == "table" and #l > 0 then
-				memory.write_u8(InputBufferRemote, l[3])
 				if l[4] <= 0 and waitingforround and l[5] then
 					waitingforround = 0
 					return
@@ -112,6 +168,14 @@ local function sendhand()
 			opponent:send("2,3,"..tostring(memory.read_u8(InputBufferLocal))) -- Player Input Delay
 			opponent:send("2,4,"..tostring(0x3C))
 			opponent:send("2,5,"..tostring(waitingforround))
+			if PLAYERNUM == 1 then
+				opponent:send("2,6,"..tostring(memory.read_u16_le(PlayerHPLocal))) -- Player HP
+			else
+				opponent:send("2,6,"..tostring(memory.read_u16_le(PlayerHPRemote))) -- Player HP
+			end
+			opponent:send("2,7,"..tostring(0x02009730)) -- RNG #1
+			opponent:send("2,8,"..tostring(0x02009800)) -- RNG #2
+			opponent:send("2,9,"..tostring(memory.read_u16_le(0x0203b380))) -- Battle Timestamp Value
 			opponent:send("loadround")
 			return
 		end
@@ -138,26 +202,36 @@ local function delaybattlestart()
 	if memory.readbyte(0x0200188F) == 0x0B then
 		thisispvp = 1
 		if #c == 0 then
-			memory.writebyte(0x020097F8,0x4)
+			memory.writebyte(SceneIndicator,0x4)
 			waitingforpvp = 1
+			gui.drawText(20, y_center - 20, "search routine", "white", nil, nil, nil, nil,"middle")
+			gui.drawText(20, y_center + 0, "find: [ Netbattler ]", "white", nil, nil, nil, nil,"middle")
 		else
 			if waitingforpvp == 1 then
 				waitingforpvp = 0
 				if PLAYERNUM == 1 then
-					delaybattletimer = 30
+					delaybattletimer = 60
 				else
 					local ping = 0
 					if type(c[1]) == "table" then
 						ping = math.min(30, math.abs(math.floor(((socket.gettime()*1000/60) % 100)) - c[1][5]))
 					end
-					delaybattletimer = 30 - ping
+					delaybattletimer = 60 - ping
+				end
+				if type(l) == "table" and #l > 0 then
+					if PLAYERNUM == 2 then
+						memory.write_u32_le(0x02009730, l[7])
+						memory.write_u32_le(0x02009800, l[8])
+					end
 				end
 			end
 			if waitingforpvp == 0 and c[1][4] == 0 and delaybattletimer > 0 then
 				delaybattletimer = delaybattletimer - 1
-				memory.writebyte(0x020097F8,0x4)
+				memory.writebyte(SceneIndicator,0x4)
+				gui.drawText(x_center, y_center - 10, "Battle routine, set", "white", nil, nil, nil, "center","middle")
+				gui.drawText(x_center, y_center + 10, "Execute!", "white", nil, nil, nil, "center","middle")
 			elseif delaybattletimer > 0 then
-				memory.writebyte(0x020097F8,0x4)
+				memory.writebyte(SceneIndicator,0x4)
 			end
 			if #c > 1 then
 				table.remove(c,1)
@@ -170,40 +244,68 @@ end
 event.onmemoryexecute(delaybattlestart,0x080048CC,"DelayBattle")
 
 local function applyp2inputs()
-	if thisispvp == 1 then
+	if thisispvp == 1 and not(resimulating) then
 		-- Sync Player Information
 		if type(c) == "table" and #c > 0 and type(c[1]) == "table" and #c[1] == 5 then
 			iBacklog = 1
-            while true do
-                local iCheck = memory.read_u8(InputStackRemote + 0x1 + (iBacklog*0x10) )
-                if iCheck == 0 then
-                    break
-                end
-                iBacklog = iBacklog + 1
-            end
-            if iBacklog > #c then
-                iWriteCount = #c
-            else
-                iWriteCount = iBacklog
-            end 
-            while iWriteCount > 0 do
-                iWriteCount = iWriteCount - 1
-                if type(c[#c - iWriteCount][2]) == "number" then
-                    memory.write_u32_le(InputStackRemote + iWriteCount*0x10, c[#c - iWriteCount][2]) --Player Button Inputs
-                    table.remove(c,#c - iWriteCount)
-                end
-            end
-        else
-            memory.write_u8(InputStackRemote+1,0x1)
-            local rollbackmode = nil
-            if rollbackmode then
-                local previoustimestamp = memory.read_u8(InputStackRemote)
-                memory.write_u8(InputStackRemote, math.floor((previoustimestamp + 0x1)%256))
-            end    
-        end
+			while true do
+				local iCheck = memory.read_u8(InputStackRemote + 0x1 + (iBacklog*0x10) )
+				if iCheck == 0 then
+					break
+				end
+				iBacklog = iBacklog + 1
+			end
+			if iBacklog > #c then
+				iWriteCount = #c
+			else
+				iWriteCount = iBacklog
+			end 
+
+			if rollbackmode then
+				while iWriteCount > 0 do
+					iWriteCount = iWriteCount - 1
+					if type(c[#c - iWriteCount][2]) == "number" then
+
+						--the input comparison assumes that the timestamps match up.
+						--under expected conditions, backlogged inputs ARE written over entries with matching timestamps
+						--but I still need to confirm whether the timestamps will always match based on stack position
+						local iStatus = bit.check(memory.read_u8(InputStackRemote + 0x1 + iWriteCount*0x10),1)
+						if iStatus then
+							local iGuess = memory.read_u16_le(InputStackRemote + 0x2 + iWriteCount*0x10)
+							memory.write_u32_le(InputStackRemote + iWriteCount*0x10, c[#c - iWriteCount][2])
+							--compare iGuess to the input that was just written
+							iCorrected = memory.read_u16_le(InputStackRemote + 0x2 + iWriteCount*0x10)
+							if iGuess ~= iCorrected then
+								if memory.read_u8(InputData+0x3) == 0 then
+									memory.write_u8(InputData+0x3,iWriteCount + 1)
+								end
+							end
+							table.remove(c,#c - iWriteCount)
+						else
+							memory.write_u32_le(InputStackRemote + iWriteCount*0x10, c[#c - iWriteCount][2]) --Player Button Inputs
+							table.remove(c,#c - iWriteCount)
+						end
+					end
+				end
+			else	--the non-rollback version of this loop
+				while iWriteCount > 0 do
+					iWriteCount = iWriteCount - 1
+					if type(c[#c - iWriteCount][2]) == "number" then
+						memory.write_u32_le(InputStackRemote + iWriteCount*0x10, c[#c - iWriteCount][2]) --Player Button Inputs
+						table.remove(c,#c - iWriteCount)
+					end
+				end
+			end
+		else
+			memory.write_u8(InputStackRemote+1,0x1)
+			if rollbackmode then
+				local previoustimestamp = memory.read_u8(InputStackRemote)
+				memory.write_u8(InputStackRemote, math.floor((previoustimestamp + 0x1)%256))
+			end    
+		end
 	end
 end
-event.onmemoryexecute(applyp2inputs,0x080085A2,"ApplyP2Inputs")
+event.onmemoryexecute(applyp2inputs,0x08008800,"ApplyP2Inputs")
 
 local function closebattle()
 	if opponent ~= nil then
@@ -223,13 +325,13 @@ tcp:settimeout(1 / (16777216 / 280896),'b')
 if PLAYERNUM == 1 then
 	tcp:bind(HOST_IP, HOST_PORT)
 	local server, err = nil, nil
-	while client == nil do
+	while connectedclient == nil do
 		if thisispvp == 1 then
 			while server == nil do
 				server, err = tcp:listen(1)
 			end
 			if server ~= nil then
-				client = tcp:accept()
+				connectedclient = tcp:accept()
 			end
 		end
 		emu.frameadvance()
@@ -238,9 +340,9 @@ if PLAYERNUM == 1 then
 -- Client
 else
 	local err = nil
-	while client == nil do
+	while connectedclient == nil do
 		if thisispvp == 1 then
-			client, err = tcp:connect(HOST_IP, HOST_PORT)
+			connectedclient, err = tcp:connect(HOST_IP, HOST_PORT)
 		end
 		emu.frameadvance()
 	end
@@ -256,8 +358,8 @@ end
 opponent = socket.udp()
 opponent:settimeout(1 / (16777216 / 280896))
 if PLAYERNUM == 1 then
-	ip, port = client:getpeername()
-	client:close()
+	ip, port = connectedclient:getpeername()
+	connectedclient:close()
 	tcp:close()
 	opponent:setsockname(HOST_IP, HOST_PORT)
 	opponent:setpeername(ip, port)
@@ -269,7 +371,7 @@ else
 end
 
 -- Finalize Connection
-if client then
+if connectedclient then
 	connected = true
 	print("Connected!")
 end
@@ -307,7 +409,7 @@ co = coroutine.create(function()
 					s = t
 					t = {}
 				end
-				if data == "loadround" and #t == 5 then -- Player Load Round Timer
+				if data == "loadround" and #t == 9 then -- Player Load Round Timer
 					l = t
 					t = {}
 				end
@@ -332,9 +434,9 @@ co = coroutine.create(function()
 			part = nil
 			acked = false
 			timedout = timedout + 1
-			if timedout >= memory.read_u8(InputBufferRemote) then
+			if timedout >= memory.read_u8(InputBufferRemote) + saferollback then
 				emu.yield()
-				if timedout >= 60*3 then
+				if timedout >= 60*5 then
 					connected = nil
 					acked = false
 					break
@@ -348,13 +450,13 @@ end)
 while true do
 	
 	-- Weird Netcode Stuff
-	if opponent ~= nil and connected then
+	if opponent ~= nil and connected and not(resimulating) then
 	
 		-- Send Data to Opponent
 		opponent:send("ack")
 		opponent:send("0,1,"..tostring(PLAYERNUM)) -- Player Number
 		opponent:send("0,2,"..tostring(memory.read_u32_le(InputStackLocal))) -- Player Control Inputs
-		opponent:send("0,3,"..tostring(memory.read_u8(0x020097F8))) -- Battle Check
+		opponent:send("0,3,"..tostring(memory.read_u8(SceneIndicator))) -- Battle Check
 		opponent:send("0,4,"..tostring(waitingforpvp)) -- Waiting for PVP Value
 		opponent:send("0,5,"..tostring(math.floor((socket.gettime()*1000/60) % 100))) -- Socket Time
 		opponent:send("control")
@@ -370,6 +472,14 @@ while true do
 			opponent:send("2,4,"..tostring(0x3C))
 		end
 		opponent:send("2,5,"..tostring(waitingforround))
+		if PLAYERNUM == 1 then
+			opponent:send("2,6,"..tostring(memory.read_u16_le(PlayerHPLocal))) -- Player HP
+		else
+			opponent:send("2,6,"..tostring(memory.read_u16_le(PlayerHPRemote))) -- Player HP
+		end
+		opponent:send("2,7,"..tostring(memory.read_u32_le(0x02009730))) -- RNG #1
+		opponent:send("2,8,"..tostring(memory.read_u32_le(0x02009800))) -- RNG #2
+		opponent:send("2,9,"..tostring(memory.read_u16_le(0x0203b380))) -- Battle Timestamp Value
 		opponent:send("loadround")
 		opponent:send("end")
 		
@@ -397,7 +507,73 @@ while true do
 		end
 		break
 	end
-	
+		
+	--rollback rountine
+	if thisispvp == 1 then 
+		if rollbackmode then
+		
+			--create savestates
+			if not(resimulating) then
+				table.insert(sav, 1, memorysavestate.savecorestate())
+				--delete the oldest savestate after 20 frames
+				if #sav > savcount then
+					memorysavestate.removestate(sav[#sav])
+					table.remove(sav,#sav)
+				end
+			end
+
+			--check if we need to roll back
+			local rollbackflag = memory.read_u8(InputData+0x3)
+				if rollbackflag > savcount-1 then
+					rollbackflag = savcount-1
+				end
+
+			if rollbackflag > 0 then
+
+				--runs once when rollback begins, but not on subsequent rollback frames
+				if not(resimulating) then
+					resimulating = 1
+					--save the corrected input stack
+					local stacksize = memory.read_u8(InputStackSize)
+					local i = 0
+					for i=0,(stacksize*2) do
+						table.insert(FullInputStack, 1, memory.read_u32_le(InputStackLocal + i*0x8))
+					end
+
+					memorysavestate.loadcorestate(sav[rollbackflag])
+
+					for i=0,(stacksize*2) do
+						memory.write_u32_le(InputStackLocal + i*0x8,FullInputStack[#FullInputStack])
+						table.remove(FullInputStack,#FullInputStack)
+					end
+
+
+					if HideResim then
+						client.invisibleemulation(true)
+						emu.limitframerate(false)
+					end
+				end
+
+				--count down remaining rollback frames by 1
+				rollbackflag = rollbackflag - 1
+				memory.write_u8(InputData+0x3,rollbackflag)
+
+				--if it's the final rollback frame, queue it up to display the next frame
+				if rollbackflag == 0 then
+					client.invisibleemulation(false)
+				end
+
+			else
+				--default branch when not in a rollback frame
+				if resimulating then
+					--restore normal speed if just now exiting out of rollback
+					resimulating = nil
+					emu.limitframerate(true)
+				end
+			end
+		end
+	end
+
 	emu.frameadvance()
 end
 
@@ -406,6 +582,10 @@ if opponent ~= nil then
 	opponent:send("disconnect")
 	opponent:close()
 	opponent = nil
+end
+while #sav > 0 do
+	memorysavestate.removestate(sav[#sav])
+	table.remove(sav,#sav)
 end
 event.unregisterbyname("CustSync")
 event.unregisterbyname("DelayBattle")
