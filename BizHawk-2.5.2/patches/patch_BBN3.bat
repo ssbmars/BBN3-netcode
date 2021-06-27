df38f4b2015a9f0be6030e8486c20574bfa7300c
ssbmars <46971717+ssbmars@users.noreply.github.com>
2021-06-26 22:13:01 -0700

    change the name of the BBN3 patch file

--- BizHawk-2.5.2/patches/patch_BBN3.bat
+++ BizHawk-2.5.2/patches/patch_BBN3.bat
@@ -1,7 +1,7 @@
 @echo off
-@echo Generating BBN3.gba 
+@echo Generating BBN3 Online.gba 
 cd /d %~dp0
-flips -a "../patches/BBN3.bps" %1 "../Netplay/BBN3 Online.gba"
+flips -a "../patches/BBN3_Online.bps" %1 "../Netplay/BBN3 Online.gba"
 @echo Finished
 timeout 5
 exit
