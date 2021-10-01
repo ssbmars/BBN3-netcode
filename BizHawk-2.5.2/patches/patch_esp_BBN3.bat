93472ee41444305253d59f84422c03ee80ffa1e6
ssbmars <46971717+ssbmars@users.noreply.github.com>
2021-09-30 22:54:32 -0700

    update rom patches, small revamp to patching system
    Each patch no longer needs a .bat file to go along with it. There's now a universal command line template that's defined within the script and carried out via os.execute.
    (I found that everything being done within the batch files could just as easily be done directly from the os.execute command, rather than using os.execute to call the batch files.)

--- BizHawk-2.5.2/patches/patch_esp_BBN3.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-@echo Generating BBN3 Online Spanish.gba 
-cd /d %~dp0
-flips -a "../patches/BBN3_Online_Spanish.bps" %1 "../Netplay/BBN3 Online Spanish.gba"
-@echo Finished
-timeout 5
-exit
